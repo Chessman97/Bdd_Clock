7f3ba021e7c9b1e2a16cc66140143f68b081abd1
Chessman97 <47553161+Chessman97@users.noreply.github.com>
2019-04-09 13:44:51 +0600

    Создали .feature, написали сценарий и сгенерировали Step Definitions

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,22 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("Создаем экземпляр объекта будильник")
+    public void создаемЭкземплярОбъектаБудильник() {
+    }
+
+    @Given("Создаем экземпляр объекта время")
+    public void создаемЭкземплярОбъектаВремя() {
+
+    }
+
+    @When("Задали будильник с точным временем")
+    public void задалиБудильникСТочнымВременем() {
+    }
+
+    @Then("У будильника задано наше время")
+    public void уБудильникаЗаданоНашеВремя() {
+    }
+}
